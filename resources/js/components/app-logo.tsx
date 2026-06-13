32ccc7c79a91ab8fb715279e46b7d051cea2bf41
farhanyp <farhan.yudha2016@gmail.com>
2026-06-14 03:56:53 +0700

    feat: add authentication layouts, app logo components, and mailpit service to docker-compose

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,11 +1,7 @@
-import { School } from 'lucide-react';
-
 export default function AppLogo() {
     return (
         <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <School className="size-5 text-white dark:text-black" />
-            </div>
+            <img src="/ypsm.png" className="rounded-md" alt="App Logo" />
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-tight font-semibold">
                     SMK Mandiri Medan
